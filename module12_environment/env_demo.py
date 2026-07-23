ed7f9c1d4937601c0f78da90f5272dfd2e2095da
Boak123 <boluwatifeakorede17@gmail.com>
2026-07-24 00:18:09 +0100

    configuration

--- module12_environment/env_demo.py
+++ module12_environment/env_demo.py
@@ -1,12 +1,18 @@
 import os
-import path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 username = os.getenv("USERNAME")
 path = os.getenv("PATH")
 temp = os.getenv("TEMP")
 
 print(username)
-print(path)
-print(temp)
-print(os.getenv("MY_SECRET"))
-print(os.getenv("CAR_COLOR"))
+# print(path)
+# print(temp)
+# print(os.getenv("MY_SECRET"))
+# print(os.getenv("CAR_COLOR"))
+
+print(os.getenv("API_KEY"))
+print(os.getenv("SECRET_KEY"))
+print(os.getenv("DATABASE_URL"))
